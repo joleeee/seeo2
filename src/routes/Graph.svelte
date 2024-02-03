4cf6ac774dcafab4577799ca6dd807a6e06540bb
Jonas <jonas@jole.xyz>
2024-02-03 21:10:03 +0100

    remove comment

--- src/routes/Graph.svelte
+++ src/routes/Graph.svelte
@@ -112,24 +112,22 @@
         LinearScale,
         PointElement,
         CategoryScale,
-        
     );
 
     function addDatapoint() {
-        const last = source_xs[source_xs.length-1];
+        const last = source_xs[source_xs.length - 1];
         const next = last + (Math.random() - 0.5) * 4;
 
         source_xs = [...source_xs, next];
     }
 
-    let clear
+    let clear;
     $: {
-        clearInterval(clear)
-        clear = setInterval(addDatapoint, 1000)
-        // clear = setInterval(() => source_xs = [...source_xs, source_xs[source_xs.length-1] + Math.random() * 5], 1000)
+        clearInterval(clear);
+        clear = setInterval(addDatapoint, 1000);
     }
 </script>
 
 <p>Your current temp is {latest}.</p>
 
-<Line {data} options={{ responsive: true, animation: {duration: 0} }} />
+<Line {data} options={{ responsive: true, animation: { duration: 0 } }} />
