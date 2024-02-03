374cd56c63545490cf83d4052feb18b2fae5413b
Jonas <jonas@jole.xyz>
2024-02-03 20:54:34 +0100

    bamp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,5 @@
 <script>
-	import Counter from './Counter.svelte';
-	import Graph from './Graph.svelte'
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import Graph from "./Graph.svelte";
 </script>
 
 <svelte:head>
@@ -12,23 +9,9 @@
 
 <section>
 	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
+		<span class="welcome"> Status: You are alive! </span>
 	</h1>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	lmao
-
-	<Counter />
 	<Graph />
 </section>
 
@@ -51,13 +34,5 @@
 		width: 100%;
 		height: 0;
 		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
